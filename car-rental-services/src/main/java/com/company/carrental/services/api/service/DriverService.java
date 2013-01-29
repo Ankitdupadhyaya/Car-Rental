4c4e9fa0f2c88c0ca762857cb0dcba6ff95bdae7
Ankit1987 <ankitdupadhyaya@gmail.com>
2013-01-30 01:15:18 +0530

    search Driver background service

--- car-rental-services/src/main/java/com/company/carrental/services/api/service/DriverService.java
+++ car-rental-services/src/main/java/com/company/carrental/services/api/service/DriverService.java
@@ -3,13 +3,14 @@ package com.company.carrental.services.api.service;
 import java.util.List;
 
 import com.company.carrental.data.pojo.DriverMaster;
-import com.company.carrental.services.core.dto.DriverMasterDto;
 import com.company.carrental.services.core.dto.SaveDriverApplicationInputDto;
 import com.company.carrental.services.core.dto.SaveDriverApplicationOutputDto;
 import com.company.carrental.services.core.dto.SaveDriverBackgroundInputDto;
 import com.company.carrental.services.core.dto.SaveDriverBackgroundOutputDto;
 import com.company.carrental.services.core.dto.SearchDriverApplicationInputDto;
 import com.company.carrental.services.core.dto.SearchDriverApplicationOutputDto;
+import com.company.carrental.services.core.dto.SearchDriverBackgroundCheckInputDto;
+import com.company.carrental.services.core.dto.SearchDriverBackgroundCheckOutputDto;
 
 
 public interface DriverService {
@@ -23,4 +24,7 @@ public interface DriverService {
     public SaveDriverBackgroundOutputDto saveDriverBackGround(SaveDriverBackgroundInputDto saveDriverBackgroundInputDto);
     
     public SearchDriverApplicationOutputDto searchDriverApplication(SearchDriverApplicationInputDto searchDriverApplicationInputDto);
+    
+    public SearchDriverBackgroundCheckOutputDto searchDriverBackgroundCheck(SearchDriverBackgroundCheckInputDto driverBackgroundCheckInputDto);
+    
 }
